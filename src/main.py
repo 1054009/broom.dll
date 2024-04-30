a105ce95a9c3b7d70e75e45f7b89852cbc8d0e19
Zach <147861361+1054009@users.noreply.github.com>
2024-04-29 21:47:55 -0400

    Rename client to bot

--- src/main.py
+++ src/main.py
@@ -1,11 +1,6 @@
 import discord
 import config
 
-client = discord.Client(intents = config.get_intents())
+bot = discord.Client(intents = config.get_intents())
 
-@client.event
-async def on_ready():
-	print("WAHHH\nWAHHhWAHHH\nWAHHhWAHHH\nWAHHhWAHHH\nWAHHhWAHHH\nWAHHh")
-
-
-client.run(config.get_config()["token"])
+bot.run(config.get_config()["token"])
