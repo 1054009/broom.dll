5c171536c2fd5787e16b79111d4e31abefb32029
Zach <147861361+1054009@users.noreply.github.com>
2024-05-22 19:53:21 -0400

    Remove print

--- src/commands/role_persist.py
+++ src/commands/role_persist.py
@@ -1,8 +1,6 @@
 import discord
 from bot import bot
 
-print("APOKPOPGKEO")
-
 @bot.tree.command(name = "role_persist")
 async def role_persist(interaction: discord.Interaction):
 	await interaction.response.send_message("FUCK YOU!!")
