d98ebe4c02c9e88935b38a59ae46456abe67dfeb
Zach <147861361+1054009@users.noreply.github.com>
2024-05-22 19:19:33 -0400

    Create models in on_ready

--- src/events/on_ready.py
+++ src/events/on_ready.py
@@ -1,8 +1,11 @@
 from bot import bot
 from database.engine import engine
+from database.models import Base
 
 @bot.event
 async def on_ready():
 	print("Here I go!")
 
 	engine.connect()
+
+	Base.metadata.create_all(engine)
